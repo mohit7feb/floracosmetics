1f760638247e5731ec4a2b57c8e00c165fb57878
mohit7feb <45337317+mohit7feb@users.noreply.github.com>
2024-07-29 18:58:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <title>Flora - Skincare Products Ecommerce Website Template</title>
+  <title>Flora Cosmetics Home</title>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
